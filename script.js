12fecf0d907b22acb3934558aa2f8bb6b5c046a5
Florian Maneyrol <florianmaneyrol@gmail.com>
2016-02-12 19:14:49 +0100

    hamburger menu

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-$(".tg").click(function() {  
-  $(".tg-dropdown").slideToggle(200).addClass("visible"); 
+// $(".tg").click(function() {  
+//   $(".tg-dropdown").slideToggle(200).addClass("visible"); 
+// });
+
+$(".tg").click(function() {
+  $(this).toggleClass('active');
+  $(".tg-dropdown").toggleClass("visible"); 
+});
+
+
+$(document).ready(function(){
+	$('#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
 });
